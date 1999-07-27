21664af4c7fe91733e151e82ec606b79e8667287
nugget <nugget>
1999-07-27 21:17:10 +0000

    Doubled top fields to store Y and O values.

--- bootstrap/STATS_dailies.sql
+++ bootstrap/STATS_dailies.sql
@@ -1,4 +1,4 @@
-# $Id: STATS_dailies.sql,v 1.2 1999/07/27 21:04:52 nugget Exp $
+# $Id: STATS_dailies.sql,v 1.3 1999/07/27 21:17:10 nugget Exp $
 #
 # STATS_dailies
 #
@@ -20,14 +20,18 @@ go
 
 create table STATS_dailies
 (
-  date            smalldatetime,
-  blocks          numeric(10,0),
-  participants    int,
-  top_participant int,
-  top_pblocks     numeric(10,0),
-  teams           int,
-  top_team        int,
-  top_tblocks     numeric(10,0)
+  date             smalldatetime,
+  blocks           numeric(10,0),
+  participants     int,
+  top_oparticipant int,
+  top_opblocks     numeric(10,0),
+  top_yparticipant int,
+  top_ypblocks     numeric(10,0),
+  teams            int,
+  top_oteam        int,
+  top_otblocks     numeric(10,0),
+  top_yteam        int,
+  top_ytblocks     numeric(10,0)
 )
 go
 
@@ -40,11 +44,15 @@ insert into
     date,
     sum(blocks) as blocks,
     count(id) as participants,
+    0 as top_oparticipant,
+    0 as top_opblocks,
+    0 as top_yparticipant,
+    0 as top_ypblocks,
     0 as teams,
-    0 as top_team,
-    0 as top_tblocks,
-    0 as top_participant,
-    0 as top_pblocks
+    0 as top_oteam,
+    0 as top_otblocks,
+    0 as top_yteam,
+    0 as top_ytblocks
   from RC5_64_master
   group by date
   order by date
@@ -72,10 +80,19 @@ from STATS_dailies, #teamsums
 where #teamsums.date = STATS_dailies.date
 go
 
-create index top_participant on STATS_dailies(top_participant)
+create index date on STATS_dailies(date)
 go
 
-create index top_team on STATS_dailies(top_team)
+create index top_oparticipant on STATS_dailies(top_oparticipant)
+go
+
+create index top_yparticipant on STATS_dailies(top_yparticipant)
+go
+
+create index top_oteam on STATS_dailies(top_oteam)
+go
+
+create index top_yteam on STATS_dailies(top_yteam)
 go
 
 grant insert on STATS_dailies to statproc
