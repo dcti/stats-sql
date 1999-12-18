3ffb1ca5e2c7fce91c45812267398351cc40800d
decibel <decibel>
1999-12-18 21:43:17 +0000

    Added dailies column

--- validation/daytotals.sql
+++ validation/daytotals.sql
@@ -1,37 +1,51 @@
-# $Id: daytotals.sql,v 1.2 1999/12/18 20:07:01 decibel Exp $
+# $Id: daytotals.sql,v 1.3 1999/12/18 21:43:17 decibel Exp $
 
 use stats
 go
 
-print "Dropping ${1}_daytotals"
-drop table ${1}_daytotals
+if ("${2}" != "nocreate")
+begin
+	print "Dropping ${1}_daytotals"
+	drop table ${1}_daytotals
+end
+else
+	print "Skipping table drop"
+go
+	
+if ("${2}" != "nocreate")
+begin
+	print "Creating ${1}_daytotals"
+	create table ${1}_daytotals
+	(	date smalldatetime,
+		master numeric(10,0) null,
+		platform numeric(10,0) null,
+		dailies numeric(10,0) null
+	)
+end
+else
+	print "Skipping table creation"
+go
+	
+if ("${2}" != "nocreate")
+begin
+	print "Inserting into ${1}_daytotals from ${1}_master"
+	insert into ${1}_daytotals(date, master)
+	select date, sum(blocks)
+	from ${1}_master
+	group by date
+end
+else
+	print "Skipping table insert"
 go
 
-print "Creating ${1}_daytotals"
-create table ${1}_daytotals
-(	date smalldatetime,
-	master numeric(10,0) null,
-	platform numeric(10,0) null,
-	delta numeric(10,0) null,
-	absdelta numeric(10,0) null
-)
-go
-
-print "Inserting into ${1}_daytotals from ${1}_master"
-insert into ${1}_daytotals(date, master)
-select date, sum(blocks)
-from ${1}_master
-group by date
-go
-
-print "Creating temp table"
+print "Creating platform temp table"
 select date, sum(blocks) as platform
 into #platblocks
 from ${1}_platform
 group by date
 go
 
-print "Creating index on temp table"
+print "Creating index on platform temp table"
 create unique clustered index idate on #platblocks(date)
 go
 
@@ -39,13 +53,46 @@ print "Adding data from temp table to ${1}_daytotals"
 update ${1}_daytotals
 set platform = pb.platform
 from #platblocks pb
-where pb.date=${1}_daytotals.date)
+where pb.date=${1}_daytotals.date
 go
 
-# code from bwilson to insert any rows from _platform that aren't in _master :)
+print "Inserting platform rows not found in master"
+insert ${1}_daytotals (date, platform)
+  select date, platform
+  from #platblocks
+  where date not in (select date from ${1}_daytotals)
+go
+drop table #platblocks
+go
 
-print "Updating extra fields in ${1}_daytotals"
+
+
+print "Creating dailies temp table"
+select date, sum(blocks) as dailies
+into #dailyblocks
+from ${1}_dailies
+group by date
+go
+
+print "Creating index on dailies temp table"
+create unique clustered index idate on #dailyblocks(date)
+go
+
+print "Adding data from temp table to ${1}_daytotals"
 update ${1}_daytotals
-set delta = master - platform, absdelta = abs(master - platform)
+set dailies = pb.dailies
+from #dailyblocks pb
+where pb.date=${1}_daytotals.date
 go
+
+print "Inserting dailies rows not found in master"
+insert ${1}_daytotals (date, dailies)
+  select date, dailies
+  from #dailyblocks
+  where date not in (select date from ${1}_daytotals)
+go
+drop table #dailyblocks
+go
+
+
 
