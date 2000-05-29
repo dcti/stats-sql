70301c2149ff292a1906109cae1904301c91a5a9
bwilson <bwilson>
2000-05-29 22:03:33 +0000

    Removed duplicate code

--- bootstrap/OGR_Team_Members.sql
+++ bootstrap/OGR_Team_Members.sql
@@ -1,25 +1,3 @@
-print 'Creating table Team_Members'
-go
-if object_id('Team_Members') is not NULL
-begin
-	exec ('if not exists (select * from Team_Members) begin drop table Team_Members end')
-end
-go
-create table Team_Members
-(
-	PROJECT_ID	tinyint,
-	ID		int,
-	TEAM_ID		int,
-	FIRST_DATE	smalldatetime,
-	LAST_DATE	smalldatetime,
-	WORK_UNITS	numeric (20,0)
-)
-go
-create index main on Team_Members (TEAM_ID, WORK_UNITS)
-go
-
-grant select on Team_Members to public
-go
-
-print 'Finished.'
-go
+/*
+** File Team_Members.sql contains this code
+*/
