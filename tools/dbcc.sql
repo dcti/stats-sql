07a001c7254d320d155c034823b26086f4e1b19d
decibel <decibel>
2000-08-30 12:34:09 +0000

    Added some nicer logging.

--- tools/dbcc.sql
+++ tools/dbcc.sql
@@ -1,4 +1,4 @@
--- $Id: dbcc.sql,v 1.3 2000/08/02 18:47:07 decibel Exp $
+-- $Id: dbcc.sql,v 1.4 2000/08/30 12:34:09 decibel Exp $
 -- Run several dbcc checks in a single file, to make logging easy.
 --
 -- Accepts:
@@ -7,9 +7,32 @@
 use ${1}
 go
 
+select convert(varchar(40), getdate()) + ": Begin checkcatalog on ${1}"
+print "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
+print ""
+go -h -f
 dbcc checkcatalog
 go
+
+print ""
+print "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
+select convert(varchar(40), getdate()) + ": Begin checkalloc on ${1}"
+print "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
+print ""
+go -h -f
 dbcc checkalloc
 go
+
+print ""
+print "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
+select convert(varchar(40), getdate()) + ": Begin checkdb on ${1}"
+print "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
+print ""
+go -h -f
 dbcc checkdb
 go
+
+print ""
+print "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
+select convert(varchar(40), getdate()) + ": Finished checks on ${1}"
+go -h -f
