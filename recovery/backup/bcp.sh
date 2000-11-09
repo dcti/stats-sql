edc7ea75b057c4e5de2c19d458ddc4c0d2848f41
decibel <decibel>
2000-11-09 09:42:40 +0000

    Add some command line options

--- recovery/backup/bcp.sh
+++ recovery/backup/bcp.sh
@@ -1,18 +1,33 @@
 #!/bin/sh
 #
-# $Id: bcp.sh,v 1.2 2000/11/06 05:55:58 decibel Exp $
+# $Id: bcp.sh,v 1.3 2000/11/09 09:42:40 decibel Exp $
 
 table_list='tables.txt'
 savedir='./'
 sqsh_flags='-w 256'
+nobcp=false
 
 #Source the config file
 if [ -r bcp.conf ]; then
 	. bcp.conf
 else
 	echo "Unable to read config file!" 2>&1
-	die
+	exit
 fi
+
+while [ x"$1" != x ]; do
+	case $1 in
+	    -n)	nobcp=true
+		shift
+		continue;;
+	    -t) table_list=$2
+		shift
+		shift
+		continue;;
+	     *) echo "Unknown command $1"
+		exit
+	esac
+done
 
 for table in `cat $table_list`
 do
@@ -21,9 +36,11 @@ do
 	sqsh -S $server -U $user -P $password ${sqsh_flags} -i tabledef.sql $table > ${savedir}${table}.def
 	echo "getting table def..."
 	sqsh -S $server -U $user -P $password ${sqsh_flags} -i info.sql $table > ${savedir}${table}.info
-	echo "bcp'ing..."
-	${SYBASE}/bin/bcp ${table} out ${savedir}${table}.bcp -n -S${server} -U${user} -P${password} | grep -v \
-		'1000 rows successfully bulk-copied'
+	if [ $nobcp = false ]; then
+		echo "bcp'ing..."
+		${SYBASE}/bin/bcp ${table} out ${savedir}${table}.bcp -n -S${server} -U${user} -P${password} | grep -v \
+			'1000 rows successfully bulk-copied'
+	fi
 	echo "done."
 	echo
 	echo
