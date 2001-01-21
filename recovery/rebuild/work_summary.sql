d7594d1fff93c031d2fe289b440d289e30dc0395
decibel <decibel>
2001-01-21 17:23:21 +0000

    Add a WORK_YESTERDAY column for ranking purposes.

--- recovery/rebuild/work_summary.sql
+++ recovery/rebuild/work_summary.sql
@@ -1,6 +1,6 @@
 #!/usr/bin/sqsh -i
 #
-# $Id: work_summary.sql,v 1.8 2000/11/29 08:34:47 decibel Exp $
+# $Id: work_summary.sql,v 1.9 2001/01/21 17:23:21 decibel Exp $
 #
 # Creates a summary table containing all work for a project
 #
@@ -24,11 +24,12 @@ create table #WorkSummary (
 	FIRST_DATE smalldatetime,
 	LAST_DATE smalldatetime,
 	WORK_TOTAL numeric(20,0),
-	WORK_TODAY numeric(20,0)
+	WORK_TODAY numeric(20,0),
+	WORK_YESTERDAY numeric(20,0)
 )
 go
-insert into #WorkSummary (PROJECT_ID, ID, TEAM_ID, FIRST_DATE, LAST_DATE, WORK_TOTAL, WORK_TODAY)
-	select ${1}, ID, TEAM_ID, min(DATE), max(DATE), sum(WORK_UNITS), 0
+insert into #WorkSummary (PROJECT_ID, ID, TEAM_ID, FIRST_DATE, LAST_DATE, WORK_TOTAL, WORK_TODAY, WORK_YESTERDAY)
+	select ${1}, ID, TEAM_ID, min(DATE), max(DATE), sum(WORK_UNITS), 0, 0
 	from Email_Contrib
 	where PROJECT_ID = ${1}
 	group by ID, TEAM_ID
@@ -36,8 +37,10 @@ go
 
 print "Update for work today"
 declare @last smalldatetime
+declare @yest smalldatetime
 select @last=max(LAST_DATE)
 	from #WorkSummary
+select @yest = dateadd(day, -1, @last)
 update #WorkSummary
 	set WORK_TODAY = ec.WORK_UNITS
 	from Email_Contrib ec
@@ -45,6 +48,13 @@ update #WorkSummary
 		and ec.ID = #WorkSummary.ID
 		and ec.TEAM_ID = #WorkSummary.TEAM_ID
 		and ec.DATE = @last
+update #WorkSummary
+	set WORK_TODAY = ec.WORK_UNITS
+	from Email_Contrib ec
+	where ec.PROJECT_ID = #WorkSummary.PROJECT_ID
+		and ec.ID = #WorkSummary.ID
+		and ec.TEAM_ID = #WorkSummary.TEAM_ID
+		and ec.DATE = @yest
 go
 
 print "Update for retire_to"
@@ -60,14 +70,15 @@ create table WorkSummary_${1} (
 	FIRST_DATE smalldatetime,
 	LAST_DATE smalldatetime,
 	WORK_TOTAL numeric(20,0),
-	WORK_TODAY numeric(20,0)
+	WORK_TODAY numeric(20,0),
+	WORK_YESTERDAY numeric(20,0)
 )
 go
 
 print "Second pass summary"
-insert into WorkSummary_${1} (ID, TEAM_ID, FIRST_DATE, LAST_DATE, WORK_TOTAL, WORK_TODAY)
+insert into WorkSummary_${1} (ID, TEAM_ID, FIRST_DATE, LAST_DATE, WORK_TOTAL, WORK_TODAY, WORK_YESTERDAY)
 	select ws.ID, ws.TEAM_ID, min(FIRST_DATE) as FIRST_DATE, max(LAST_DATE) as LAST_DATE,
-		sum(WORK_TOTAL) as WORK_TOTAL, sum(WORK_TODAY) as WORK_TODAY
+		sum(WORK_TOTAL) as WORK_TOTAL, sum(WORK_TODAY) as WORK_TODAY, sum(WORK_YESTERDAY) as WORK_YESTERDAY
 	from #WorkSummary ws, Stats_Participant sp
 	where ws.ID = sp.ID
 		and sp.LISTMODE <= 9
