8794a520fe1c6e424183b44525e8810edb25c557
bwilson <bwilson>
2001-12-31 20:13:16 +0000

    Add comments, select from Platform_Contrib instead of Email_Contrib for speed

--- procedures/lastupdate/simple.sql
+++ procedures/lastupdate/simple.sql
@@ -1,3 +1,12 @@
+/*
+** p_lastupdate2
+** Replacement for p_lastupdate, without all the overhead of multiple procs
+** and unnecessary parameters.
+** Procedure specifically ignores the differences between when email, team
+** and project are last updated, as showing this to the users only serves to
+** cause confusion.
+*/
+
 if object_id('p_lastupdate2') is not NULL
 begin
 	drop procedure p_lastupdate2
@@ -17,6 +26,18 @@ begin
 		raiserror 99999 "Invalid project specified"
 	end
 
+	/* 
+	** When RC5_64 is merged into the main tables, this collapses to just
+	** the select statement from the else
+	*/
+	
+	/*
+	** We may want to use Projects.LAST_STATS_DATE instead, if we can decide
+	** that will always, only represent the last full stats run to complete.
+	** If that field represents the upload date, then we should have a new
+	** field for this purpose.
+	*/
+	
 	if @project_id = 5
 	begin
 		select @lastdate = max(date)
@@ -25,10 +46,15 @@ begin
 	else
 	begin
 		select @lastdate = max(DATE)
-			from Email_Contrib
+			from Platform_Contrib
 			where PROJECT_ID = @project_id
 	end
 	
+	/*
+	** This is probably a bit of overkill in this scenario, but makes
+	** sense in the general case
+	*/
+
 	select @lastdate as lastupdate
 end
 go
