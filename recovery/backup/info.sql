67868e654e42234246ce60a8a1846324731e832f
decibel <decibel>
2002-10-04 16:36:17 +0000

    Fix script

--- recovery/backup/info.sql
+++ recovery/backup/info.sql
@@ -1,6 +1,7 @@
--- $Id: info.sql,v 1.4 2002/10/04 16:32:19 decibel Exp $
+-- $Id: info.sql,v 1.5 2002/10/04 16:36:17 decibel Exp $
 
 select getdate()
+go -f
 sp_help "${1}"
 go -f
 sp__objspace "${1}"
