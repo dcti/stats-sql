d4c02c8fb875b374855e6885214599beb7e0ee40
decibel <decibel>
2002-10-06 22:53:01 +0000

    Support new Email_Contrib_Last_Update table

--- procedures/lastupdate.sh
+++ procedures/lastupdate.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Definitions
-version='$Id: lastupdate.sh,v 1.8 2002/10/06 22:43:57 decibel Exp $'
+version='$Id: lastupdate.sh,v 1.9 2002/10/06 22:53:01 decibel Exp $'
 sqlpath=lastupdate/
 deletescript=${sqlpath}delete.sql
 insertscript=${sqlpath}template
@@ -71,7 +71,7 @@ setupsql csc_p csc_platform date
 
 setupsql2 new_e Email_Rank LAST_DATE
 setupsql2 new_t Team_Rank LAST_DATE
-setupsql2 new_m Email_Contrib DATE
+setupsql2 new_m Email_Contrib_Last_Update LAST_DATE
 setupsql2 new_p Platform_Contrib DATE
 
 [ -f ${sqlpath}depends ] && echo && cat ${sqlpath}depends
