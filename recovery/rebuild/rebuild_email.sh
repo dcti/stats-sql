37902c7328444594a94dd3ea95c19b811f95ee64
decibel <decibel>
2002-10-07 06:48:33 +0000

    Reverse order of arguments

--- recovery/rebuild/rebuild_email.sh
+++ recovery/rebuild/rebuild_email.sh
@@ -1,3 +1,3 @@
-# $Id: rebuild_email.sh,v 1.2 2002/04/14 05:07:48 decibel Exp $
+# $Id: rebuild_email.sh,v 1.3 2002/10/07 06:48:33 decibel Exp $
 
-sqsh $2 -i email_rank.sql $1 && sqsh $2 -i ../../../stats-proc/daily//em_rank.sql $1 && sqsh $2 -i email_rank_2.sql $1 && sqsh $2 -i ../../../stats-proc/daily/em_rank.sql $1
+sqsh $1 -i email_rank.sql $2 && sqsh $1 -i ../../../stats-proc/daily/em_rank.sql $2 && sqsh $1 -i email_rank_2.sql $2 && sqsh $1 -i ../../../stats-proc/daily/em_rank.sql $2
