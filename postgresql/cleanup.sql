06592b7341498b6431c02263397e0926a5939bdb
decibel <decibel>
2003-04-20 22:05:21 +0000

    last_hourly_date -> last_date, drop other last_blah_date fields

--- postgresql/cleanup.sql
+++ postgresql/cleanup.sql
@@ -1,4 +1,4 @@
--- $Id: cleanup.sql,v 1.9 2003/04/14 21:47:41 decibel Exp $
+-- $Id: cleanup.sql,v 1.10 2003/04/20 22:05:21 decibel Exp $
 \set ON_ERROR_STOP 1
 
 select now() as start into temp start_time;
@@ -10,6 +10,12 @@ alter table projects drop column deprecated_fields ;
 alter table projects drop column work_unit_name;
 alter table projects drop column dist_unit_scale;
 alter table projects drop column work_unit_scale;
+
+alter table project_statsrun rename column last_hourly_date to last_date;
+alter table project_statsrun drop column last_master_date;
+alter table project_statsrun drop column last_email_date;
+alter table project_statsrun drop column last_team_date;
+alter table project_statsrun drop column last_summary_date;
 
 vacuum full analyze verbose;
 
