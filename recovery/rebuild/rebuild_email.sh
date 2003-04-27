1e7280453f2bee83cb598994e7bc840f72e252ac
decibel <decibel>
2003-04-27 22:06:56 +0000

    cleanup .sh scripts

--- recovery/rebuild/rebuild_email.sh
+++ recovery/rebuild/rebuild_email.sh
@@ -1,13 +1,14 @@
-# $Id: rebuild_email.sh,v 1.3.2.1 2003/04/27 12:26:59 decibel Exp $
+#!/bin/sh
+# $Id: rebuild_email.sh,v 1.3.2.2 2003/04/27 22:06:56 decibel Exp $
 
-if [ x$1 == x ]; then
+if [ x$1 = x ]; then
     echo Must specify project ID!
     exit 1;
 fi
-if [ x$2 == x ]; then
+if [ x$2 = x ]; then
     database=stats
 else
     database=$2
 fi
 
-psql  -d $database -v ProjectID=$1 -f email_rank.sql && psql  -d $database -v ProjectID=$1 -f ../../../stats-proc/daily/em_rank.sql && psql  -d $database -v ProjectID=$1 -f email_rank_2.sql  && psql  -d $database -v ProjectID=$1 -f ../../../stats-proc/daily/em_rank.sql 
+psql -d $database -v ProjectID=$1 -f email_rank.sql && psql -d $database -v ProjectID=$1 -f ../../../stats-proc/daily/em_rank.sql && psql -d $database -v ProjectID=$1 -f email_rank_2.sql  && psql -d $database -v ProjectID=$1 -f ../../../stats-proc/daily/em_rank.sql 
--- recovery/rebuild/rebuild_team.sh
+++ recovery/rebuild/rebuild_team.sh
@@ -1,13 +1,14 @@
-# $Id: rebuild_team.sh,v 1.2.2.1 2003/04/27 12:26:59 decibel Exp $
+#!/bin/sh
+# $Id: rebuild_team.sh,v 1.2.2.2 2003/04/27 22:06:56 decibel Exp $
 
-if [ x$1 == x ]; then
+if [ x$1 = x ]; then
     echo Must specify project ID!
     exit 1;
 fi
-if [ x$2 == x ]; then
+if [ x$2 = x ]; then
     database=stats
 else
     database=$2
 fi
 
-psql  -d $database -v ProjectID=$1 -f team_members.sql  && psql  -d $database -v ProjectID=$1 -f team_rank.sql  && psql  -d $database -v ProjectID=$1 -f ../../../stats-proc/daily/tm_rank.sql  && psql  -d $database -v ProjectID=$1 -f team_rank_2.sql  && psql  -d $database -v ProjectID=$1 -f ../../../stats-proc/daily/tm_rank.sql 
+psql -d $database -v ProjectID=$1 -f team_members.sql  && psql -d $database -v ProjectID=$1 -f team_rank.sql  && psql -d $database -v ProjectID=$1 -f ../../../stats-proc/daily/tm_rank.sql  && psql -d $database -v ProjectID=$1 -f team_rank_2.sql  && psql -d $database -v ProjectID=$1 -f ../../../stats-proc/daily/tm_rank.sql 
