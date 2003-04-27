4a3485c2cb239f93455d3a07254f4c4c515b383b
decibel <decibel>
2003-04-27 22:10:49 +0000

    Fixor script

--- recovery/rebuild/work_summary.sh
+++ recovery/rebuild/work_summary.sh
@@ -1,13 +1,14 @@
-# $Id: work_summary.sh,v 1.1.2.1 2003/04/27 22:09:48 decibel Exp $
+#!/bin/sh
+# $Id: work_summary.sh,v 1.1.2.2 2003/04/27 22:10:49 decibel Exp $
 
-if [ x$1 == x ]; then
+if [ x$1 = x ]; then
     echo Must specify project ID!
     exit 1;
 fi
-if [ x$2 == x ]; then
+if [ x$2 = x ]; then
     database=stats
 else
     database=$2
 fi
 
-psql  -d $database -v ProjectID=$1 -f work_summary.sql
+psql -d $database -v ProjectID=$1 -f work_summary.sql
