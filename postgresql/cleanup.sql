a322baf7560992df1459c3179342817d199335f0
decibel <decibel>
2003-06-13 01:43:09 +0000

    Do an update to force reclaim of space for dropped columns

--- postgresql/cleanup.sql
+++ postgresql/cleanup.sql
@@ -1,4 +1,4 @@
--- $Id: cleanup.sql,v 1.10 2003/04/20 22:05:21 decibel Exp $
+-- $Id: cleanup.sql,v 1.11 2003/06/13 01:43:09 decibel Exp $
 \set ON_ERROR_STOP 1
 
 select now() as start into temp start_time;
@@ -10,12 +10,16 @@ alter table projects drop column deprecated_fields ;
 alter table projects drop column work_unit_name;
 alter table projects drop column dist_unit_scale;
 alter table projects drop column work_unit_scale;
+-- Do update to force reclaming space from dropped columns
+UPDATE projects SET project_id = project_id;
 
 alter table project_statsrun rename column last_hourly_date to last_date;
 alter table project_statsrun drop column last_master_date;
 alter table project_statsrun drop column last_email_date;
 alter table project_statsrun drop column last_team_date;
 alter table project_statsrun drop column last_summary_date;
+-- Do update to force reclaming space from dropped columns
+UPDATE project_statsrun SET project_id = project_id;
 
 vacuum full analyze verbose;
 
