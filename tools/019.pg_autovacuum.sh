5e72924a03a0d250aa5145a013ac919e1b175e7a
decibel <decibel>
2004-04-22 18:36:28 +0000

    Script to start pg_autovacuum

---
+++ tools/019.pg_autovacuum.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+# $Id: 019.pg_autovacuum.sh,v 1.1 2004/04/22 18:36:28 decibel Exp $
+
+CONFIG=-d 2
+LOGFILE=/var/log/pg_autovacuum.log
+
+PREFIX=/usr/local
+PGBIN=${PREFIX}/bin
+
+case $1 in
+start)
+    [ -x ${PGBIN}/pg_autovacuum ] && {
+	[ x${LOGFILE} != x ] && CONFIG=${CONFIG} -L ${LOGFILE}
+	echo -n ' pg_autovacuum'
+	su -l pgsql -c "exec ${PREFIX}/bin/pg_autovacuum -D ${CONFIG}"
+    }
+    ;;
+
+*)
+    echo "usage: `basename $0` {start}" >&2
+    exit 64
+    ;;
+esac
