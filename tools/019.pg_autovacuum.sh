08b1b1b41e3e37f3259272244c1b1ad5f4e0eda1
decibel <decibel>
2004-04-22 18:40:48 +0000

    Fix boogs

--- tools/019.pg_autovacuum.sh
+++ tools/019.pg_autovacuum.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #
-# $Id: 019.pg_autovacuum.sh,v 1.1 2004/04/22 18:36:28 decibel Exp $
+# $Id: 019.pg_autovacuum.sh,v 1.2 2004/04/22 18:40:48 decibel Exp $
 
-CONFIG=-d 2
+CONFIG="-d 2"
 LOGFILE=/var/log/pg_autovacuum.log
 
 PREFIX=/usr/local
@@ -11,7 +11,7 @@ PGBIN=${PREFIX}/bin
 case $1 in
 start)
     [ -x ${PGBIN}/pg_autovacuum ] && {
-	[ x${LOGFILE} != x ] && CONFIG=${CONFIG} -L ${LOGFILE}
+	[ x${LOGFILE} != x ] && CONFIG="${CONFIG} -L ${LOGFILE}"
 	echo -n ' pg_autovacuum'
 	su -l pgsql -c "exec ${PREFIX}/bin/pg_autovacuum -D ${CONFIG}"
     }
