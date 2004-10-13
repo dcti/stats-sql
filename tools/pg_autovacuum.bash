415180c43410bee0d161334a616a2696e4316cb4
decibel <decibel>
2004-10-13 23:36:26 +0000

    Add ability to send HUP to pg_autovacuum. Drop retry sleep down to 10 seconds

--- tools/pg_autovacuum.bash
+++ tools/pg_autovacuum.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# $Id: pg_autovacuum.bash,v 1.1 2004/10/13 22:08:20 decibel Exp $
+# $Id: pg_autovacuum.bash,v 1.2 2004/10/13 23:36:26 decibel Exp $
 
 CONFIG=""
 LOGFILE=/var/log/pg_autovacuum
@@ -10,6 +10,20 @@ PGBIN=${PREFIX}/bin
 
 log () {
     [ x${LOGFILE} != x ] && echo $@ >> ${LOGFILE}
+}
+
+sendsig () {
+    if [ x${running_pid} != x ]; then
+        if [ x$2 != x ]; then
+            log "Signal $1 received, sending signal $2 to PID ${running_pid}"
+            kill -${2} ${running_pid} 2> /dev/null
+        else
+            log "Signal $1 received, sending to PID ${running_pid}"
+            kill -${1} ${running_pid} 2> /dev/null
+        fi
+    else
+        log "signal $1 received but \$running_pid isn't set; ignoring"
+    fi
 }
 
 sigdie () {
@@ -27,6 +41,7 @@ sigdie () {
     exit
 }
 
+trap 'sendsig 1' 1
 trap 'sigdie 2 15' 2
 trap 'sigdie 9' 9
 trap 'sigdie 15' 15
@@ -46,7 +61,7 @@ while true; do
     wait
 
     # Sleep a bit before we start checking incase there was an error starting up
-    sleep 30
+    sleep 10
 
     #while ( ps -p $running_pid 2> /dev/null | grep -q "${COMMAND}" ); do
     #    sleep 300
