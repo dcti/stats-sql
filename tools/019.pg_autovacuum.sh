c6cdd06c3136b8c8c13ff1c6f38417aadb1b6dbc
decibel <decibel>
2004-10-13 01:55:08 +0000

    Brand new version of autovacuum launching script

--- tools/019.pg_autovacuum.sh
+++ tools/019.pg_autovacuum.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 #
-# $Id: 019.pg_autovacuum.sh,v 1.3 2004/04/23 22:50:19 decibel Exp $
+# $Id: 019.pg_autovacuum.sh,v 1.4 2004/10/13 01:55:08 decibel Exp $
 
-CONFIG=""
-LOGFILE=/var/log/pg_autovacuum.log
-
-PREFIX=/usr/local
-PGBIN=${PREFIX}/bin
-
+RUNSCRIPT=~pgsql/pg_autovacuum.sh
 case $1 in
 start)
-    [ -x ${PGBIN}/pg_autovacuum ] && {
-	[ x${LOGFILE} != x ] && CONFIG="${CONFIG} -L ${LOGFILE}"
-	echo -n ' pg_autovacuum'
-	su -l pgsql -c "exec ${PREFIX}/bin/pg_autovacuum -D ${CONFIG}"
-    }
+    if [ -x ${RUNSCRIPT} ]; then
+        [ x${LOGFILE} != x ] && CONFIG="${CONFIG} -L ${LOGFILE}"
+        su -l pgsql -c "${RUNSCRIPT} &" > /var/run/pg_autovacuum.pid && echo -n ' pg_autovacuum'
+    else
+        echo $0: $RUNSCRIPT is not executable
+    fi
+    ;;
+stop)
+    kill `cat /var/run/pg_autovacuum.pid`
+    rm /var/run/pg_autovacuum.pid
     ;;
 
 *)
-    echo "usage: `basename $0` {start}" >&2
+    echo "usage: `basename $0` {start|stop}" >&2
     exit 64
     ;;
 esac
