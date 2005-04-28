a0008c19a5383c8d25c636ab92f8312bb95aaff7
decibel <decibel>
2005-04-28 05:33:04 +0000

    Include projects data in build

--- build/build.sh
+++ build/build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 #
-# $Id: build.sh,v 1.2 2005/01/08 02:52:24 decibel Exp $
+# $Id: build.sh,v 1.3 2005/04/28 05:33:04 decibel Exp $
 
 createdb -T template0 stats
 psql -f accounts.sql stats
 psql -f schema_dump.sql stats
 psql -f notes.sql stats
+psql -f projects.dat.sql stats
