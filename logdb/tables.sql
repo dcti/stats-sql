69e2c8c96da1d25641320c02af6a17e8e7abc928
decibel <decibel>
2005-05-09 22:43:04 +0000

    Add catch-all tables for bad project_id's. Add stuff for log_r72

--- logdb/tables.sql
+++ logdb/tables.sql
@@ -1,4 +1,4 @@
--- $Id: tables.sql,v 1.3 2005/05/09 22:17:43 decibel Exp $
+-- $Id: tables.sql,v 1.4 2005/05/09 22:43:04 decibel Exp $
 
 CREATE TABLE email (
 	email_id	serial 		PRIMARY KEY
@@ -13,6 +13,10 @@ CREATE TABLE platform (
 	, UNIQUE (os, cpu, version)
 ) WITHOUT OIDs;
 
+/*
+	R72
+*/
+
 CREATE TABLE import_r72 (
 	return_time	timestamp NOT NULL
 	, ip_address	inet NOT NULL
@@ -20,7 +24,8 @@ CREATE TABLE import_r72 (
 	, os_type	integer NOT NULL
 	, cpu_type	integer NOT NULL
 	, version	integer NOT NULL
-	, key_block	varchar(20) NOT NULL
+	, project_id	smallint NOT NULL
+	, workunit_tid	varchar(20) NOT NULL
 	, iter		smallint NOT NULL
 	, core		integer NOT NULL
 	, cmc_last	varchar(20)
@@ -37,9 +42,16 @@ CREATE TABLE log_8 (
 	, cmc_count		integer
 	, cmc_ok		smallint
 	, core			smallint NOT NULL
-	, key_block		varchar(20) NOT NULL
+	, workunit_tid		varchar(20) NOT NULL
 	, cmc_last		varchar(20)
 ) WITHOUT OIDs;
+CREATE TABLE log_r72_other WITHOUT OIDs AS
+	SELECT 0::smallint AS project_id, * FROM log_8
+;
+
+/*
+	OGR
+*/
 
 CREATE TABLE import_ogr (
 	return_time	timestamp NOT NULL
@@ -48,6 +60,8 @@ CREATE TABLE import_ogr (
 	, os_type	integer NOT NULL
 	, cpu_type	integer NOT NULL
 	, version	integer NOT NULL
+	, project_id	smallint NOT NULL
+	-- TODO create a custom ogr node datatype
 	, stub_marks	varchar(22) NOT NULL
 	, nodecount	bigint NOT NULL
 	, status	smallint
@@ -58,7 +72,7 @@ CREATE TABLE log_24 (
 	, ip_address		inet NOT NULL
 	, email_id		integer NOT NULL REFERENCES email
 	, platform_id		integer NOT NULL REFERENCES platform
-	, ogr_stub		text NOT NULL
+	, workunit_tid		text NOT NULL
 	, nodecount		bigint NOT NULL
 	, status		smallint
 ) WITHOUT OIDs;
@@ -68,9 +82,13 @@ CREATE TABLE log_25 (
 	, ip_address		inet NOT NULL
 	, email_id		integer NOT NULL REFERENCES email
 	, platform_id		integer NOT NULL REFERENCES platform
-	, ogr_stub		text NOT NULL
+	, workunit_tid		text NOT NULL
 	, nodecount		bigint NOT NULL
 	, status		smallint
 ) WITHOUT OIDs;
+
+CREATE TABLE log_ogr_other WITHOUT OIDs AS
+	SELECT 0::smallint AS project_id, * FROM log_25
+;
 
 -- vi: noexpandtab ts=8 sw=8
--- logdb/views.sql
+++ logdb/views.sql
@@ -1,29 +1,78 @@
--- $Id: views.sql,v 1.5 2005/05/09 22:17:35 decibel Exp $
+-- $Id: views.sql,v 1.6 2005/05/09 22:43:04 decibel Exp $
 
--- Note, we'll need something similar for log_r72, but that can wait for now
+/*
+	R72
+*/
+
+CREATE OR REPLACE VIEW log_r72 AS
+    SELECT * FROM log_r72_other
+    UNION ALL
+    SELECT 8::smallint AS project_id, * FROM log_8
+;
+
+CREATE OR REPLACE RULE log_r72_insert_nothing AS ON INSERT TO log_r72
+    DO INSTEAD NOTHING
+;
+
+CREATE OR REPLACE RULE log_r72_insert_other AS ON INSERT TO log_r72
+    WHERE NEW.project_id NOT IN ( 8 )
+    DO INSTEAD
+        INSERT INTO log_r72_other( project_id, return_time, ip_address, email_id, platform_id
+                    , iter, cmc_count, cmc_ok, core, workunit_tid, cmc_last )
+            VALUES( NEW.project_id, NEW.return_time, NEW.ip_address, NEW.email_id, NEW.platform_id
+                    , NEW.iter, NEW.cmc_count, NEW.cmc_ok, NEW.core, NEW.workunit_tid, NEW.cmc_last )
+;
+
+CREATE OR REPLACE RULE log_r72_insert_8 AS ON INSERT TO log_r72
+    WHERE NEW.project_id = 8
+    DO INSTEAD
+        INSERT INTO log_8( return_time, ip_address, email_id, platform_id
+                    , iter, cmc_count, cmc_ok, core, workunit_tid, cmc_last )
+            VALUES( NEW.return_time, NEW.ip_address, NEW.email_id, NEW.platform_id
+                    , NEW.iter, NEW.cmc_count, NEW.cmc_ok, NEW.core, NEW.workunit_tid, NEW.cmc_last )
+;
+
+/*
+	OGR
+*/
 
 CREATE OR REPLACE VIEW log_ogr AS
+    SELECT * FROM log_ogr_other
+    UNION ALL
     SELECT 24::smallint AS project_id, * FROM log_24
     UNION ALL
     SELECT 25::smallint AS project_id, * FROM log_25
 ;
 
-CREATE OR REPLACE RULE log_ogr_insert_other AS ON INSERT TO log_ogr
+CREATE OR REPLACE RULE log_ogr_insert_nothing AS ON INSERT TO log_ogr
     DO INSTEAD NOTHING
+;
+
+CREATE OR REPLACE RULE log_ogr_insert_other AS ON INSERT TO log_ogr
+    WHERE NEW.project_id NOT IN ( 24, 25 )
+    DO INSTEAD
+        INSERT INTO log_ogr_other( project_id, return_time, ip_address, email_id, platform_id
+                    , workunit_tid, nodecount, status )
+            VALUES( NEW.project_id, NEW.return_time, NEW.ip_address, NEW.email_id, NEW.platform_id
+                    , NEW.workunit_tid, NEW.nodecount, NEW.status )
 ;
 
 CREATE OR REPLACE RULE log_ogr_insert_24 AS ON INSERT TO log_ogr
     WHERE NEW.project_id = 24
     DO INSTEAD
-        INSERT INTO log_24( return_time, ip_address, email_id, platform_id, ogr_stub_id, nodecount, status )
-            VALUES( NEW.return_time, NEW.ip_address, NEW.email_id, NEW.platform_id, NEW.ogr_stub_id, NEW.nodecount, NEW.status )
+        INSERT INTO log_24( return_time, ip_address, email_id, platform_id
+                    , workunit_tid, nodecount, status )
+            VALUES( NEW.return_time, NEW.ip_address, NEW.email_id, NEW.platform_id
+                    , NEW.workunit_tid, NEW.nodecount, NEW.status )
 ;
 
 CREATE OR REPLACE RULE log_ogr_insert_25 AS ON INSERT TO log_ogr
     WHERE NEW.project_id = 25
     DO INSTEAD
-        INSERT INTO log_25( return_time, ip_address, email_id, platform_id, ogr_stub_id, nodecount, status )
-            VALUES( NEW.return_time, NEW.ip_address, NEW.email_id, NEW.platform_id, NEW.ogr_stub_id, NEW.nodecount, NEW.status )
+        INSERT INTO log_25( return_time, ip_address, email_id, platform_id
+                    , workunit_tid, nodecount, status )
+            VALUES( NEW.return_time, NEW.ip_address, NEW.email_id, NEW.platform_id
+                    , NEW.workunit_tid, NEW.nodecount, NEW.status )
 ;
 
 -- vi: expandtab ts=4 sw=4
