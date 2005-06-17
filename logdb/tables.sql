935f6aad6727cc62c01a0256ff0599b1849b5706
decibel <decibel>
2005-06-17 22:54:02 +0000

    Genericize r72 into rc5. Add generic log view that sits on top of everything.

--- logdb/tables.sql
+++ logdb/tables.sql
@@ -1,4 +1,39 @@
--- $Id: tables.sql,v 1.7 2005/05/09 23:03:29 decibel Exp $
+-- $Id: tables.sql,v 1.8 2005/06/17 22:54:02 decibel Exp $
+
+-- Note that we should probable just create this as a temp table in the import script
+CREATE TABLE import (
+	return_time	timestamp NOT NULL
+	, ip_address	inet NOT NULL
+	, os_type	integer NOT NULL
+	, cpu_type	integer NOT NULL
+	, version	integer NOT NULL
+	, core		integer
+	, rc5_cmc_count	integer
+	, project_id	smallint NOT NULL
+	, rc5_iter	smallint
+	, ogr_status	smallint
+	, rc5_cmc_ok	smallint
+	, ogr_nodecount	bigint
+	, workunit_tid	text NOT NULL
+	, email		varchar(64) NOT NULL
+	, rc5_cmc_last	text
+) WITHOUT OIDs;
+
+CREATE TABLE log_other (
+	return_time	timestamp NOT NULL
+	, ip_address	inet NOT NULL
+	, email_id	int NOT NULL
+	, platform_id	integer NOT NULL
+	, core		integer
+	, rc5_cmc_count	integer
+	, project_id	smallint NOT NULL
+	, rc5_iter	smallint
+	, ogr_status	smallint
+	, rc5_cmc_ok	smallint
+	, ogr_nodecount	bigint
+	, workunit_tid	text NOT NULL
+	, rc5_cmc_last	text
+) WITHOUT OIDs;
 
 CREATE TABLE email (
 	email_id	serial 		PRIMARY KEY
@@ -17,68 +52,38 @@ CREATE TABLE platform (
 	R72
 */
 
-CREATE TABLE import_r72 (
-	return_time	timestamp NOT NULL
-	, ip_address	inet NOT NULL
-	, os_type	integer NOT NULL
-	, cpu_type	integer NOT NULL
-	, version	integer NOT NULL
-	, core		integer NOT NULL
-	, cmc_count	integer
-	, project_id	smallint NOT NULL
-	, iter		smallint NOT NULL
-	, cmc_ok	smallint
-	, workunit_tid	varchar(20) NOT NULL
-	, email		varchar(64) NOT NULL
-	, cmc_last	varchar(20)
-) WITHOUT OIDs;
-
 CREATE TABLE log_8 (
 	return_time		timestamp NOT NULL
 	, ip_address		inet NOT NULL
 	, email_id		integer NOT NULL REFERENCES email
 	, platform_id		integer NOT NULL REFERENCES platform
-	, cmc_count		integer
-	, cmc_ok		smallint
-	, iter			smallint NOT NULL
+	, rc5_cmc_count		integer
+	, rc5_cmc_ok		smallint
+	, rc5_iter		smallint NOT NULL
 	, core			smallint NOT NULL
 	, workunit_tid		varchar(20) NOT NULL
-	, cmc_last		varchar(20)
+	, rc5_cmc_last		varchar(20)
 ) WITHOUT OIDs;
 CREATE INDEX log_8__email_id ON log_8( email_id );
 
-CREATE TABLE log_r72_other WITHOUT OIDs AS
+CREATE TABLE log_rc5_other WITHOUT OIDs AS
 	SELECT 0::smallint AS project_id, * FROM log_8
 ;
-ALTER TABLE log_r72_other ADD CONSTRAINT log_r72_other__email_ri FOREIGN KEY email_id REFERENCES email;
-ALTER TABLE log_r72_other ADD CONSTRAINT log_r72_other__platform_ri FOREIGN KEY platform_id REFERENCES platform;
-CREATE INDEX log_r72_other__email_id ON log_r72_other( project_id, email_id );
+ALTER TABLE log_rc5_other ADD CONSTRAINT log_rc5_other__email_ri FOREIGN KEY ( email_id ) REFERENCES email;
+ALTER TABLE log_rc5_other ADD CONSTRAINT log_rc5_other__platform_ri FOREIGN KEY ( platform_id ) REFERENCES platform;
+CREATE INDEX log_rc5_other__email_id ON log_rc5_other( project_id, email_id );
 
 /*
 	OGR
 */
-
-CREATE TABLE import_ogr (
-	return_time	timestamp NOT NULL
-	, ip_address	inet NOT NULL
-	, os_type	integer NOT NULL
-	, cpu_type	integer NOT NULL
-	, version	integer NOT NULL
-	, status	smallint
-	, project_id	smallint NOT NULL
-	, nodecount	bigint NOT NULL
-	, email		varchar(64) NOT NULL
-	-- TODO create a custom ogr node datatype
-	, stub_marks	varchar(22) NOT NULL
-) WITHOUT OIDs;
 
 CREATE TABLE log_24 (
 	return_time		timestamp NOT NULL
 	, ip_address		inet NOT NULL
 	, email_id		integer NOT NULL CONSTRAINT log_24__email_ri REFERENCES email
 	, platform_id		integer NOT NULL CONSTRAINT log_24__platform_ri REFERENCES platform
-	, status		smallint
-	, nodecount		bigint NOT NULL
+	, ogr_status		smallint
+	, ogr_nodecount		bigint NOT NULL
 	, workunit_tid		text NOT NULL
 ) WITHOUT OIDs;
 CREATE INDEX log_24__email_id ON log_24( email_id );
@@ -86,15 +91,15 @@ CREATE INDEX log_24__email_id ON log_24( email_id );
 CREATE TABLE log_25 WITHOUT OIDs AS
 	SELECT * FROM log_24
 ;
-ALTER TABLE log_25 ADD CONSTRAINT log_25__email_ri FOREIGN KEY email_id REFERENCES email;
-ALTER TABLE log_25 ADD CONSTRAINT log_25__platform_ri FOREIGN KEY platform_id REFERENCES platform;
+ALTER TABLE log_25 ADD CONSTRAINT log_25__email_ri FOREIGN KEY ( email_id ) REFERENCES email;
+ALTER TABLE log_25 ADD CONSTRAINT log_25__platform_ri FOREIGN KEY ( platform_id ) REFERENCES platform;
 CREATE INDEX log_25__email_id ON log_25( email_id );
 
 CREATE TABLE log_ogr_other WITHOUT OIDs AS
 	SELECT 0::smallint AS project_id, * FROM log_25
 ;
-ALTER TABLE log_ogr_other ADD CONSTRAINT log_ogr_other__email_ri FOREIGN KEY email_id REFERENCES email;
-ALTER TABLE log_ogr_other ADD CONSTRAINT log_ogr_other__platform_ri FOREIGN KEY platform_id REFERENCES platform;
+ALTER TABLE log_ogr_other ADD CONSTRAINT log_ogr_other__email_ri FOREIGN KEY ( email_id ) REFERENCES email;
+ALTER TABLE log_ogr_other ADD CONSTRAINT log_ogr_other__platform_ri FOREIGN KEY ( platform_id ) REFERENCES platform;
 CREATE INDEX log_ogr_other__email_id ON log_ogr_other( project_id, email_id );
 
 -- vi: noexpandtab ts=8 sw=8
--- logdb/views.sql
+++ logdb/views.sql
@@ -1,35 +1,35 @@
--- $Id: views.sql,v 1.6 2005/05/09 22:43:04 decibel Exp $
+-- $Id: views.sql,v 1.7 2005/06/17 22:54:02 decibel Exp $
 
 /*
 	R72
 */
 
-CREATE OR REPLACE VIEW log_r72 AS
-    SELECT * FROM log_r72_other
+CREATE OR REPLACE VIEW log_rc5 AS
+    SELECT * FROM log_rc5_other
     UNION ALL
     SELECT 8::smallint AS project_id, * FROM log_8
 ;
 
-CREATE OR REPLACE RULE log_r72_insert_nothing AS ON INSERT TO log_r72
+CREATE OR REPLACE RULE log_rc5_insert_nothing AS ON INSERT TO log_rc5
     DO INSTEAD NOTHING
 ;
 
-CREATE OR REPLACE RULE log_r72_insert_other AS ON INSERT TO log_r72
+CREATE OR REPLACE RULE log_rc5_insert_other AS ON INSERT TO log_rc5
     WHERE NEW.project_id NOT IN ( 8 )
     DO INSTEAD
-        INSERT INTO log_r72_other( project_id, return_time, ip_address, email_id, platform_id
-                    , iter, cmc_count, cmc_ok, core, workunit_tid, cmc_last )
+        INSERT INTO log_rc5_other( project_id, return_time, ip_address, email_id, platform_id
+                    , rc5_iter, rc5_cmc_count, rc5_cmc_ok, core, workunit_tid, rc5_cmc_last )
             VALUES( NEW.project_id, NEW.return_time, NEW.ip_address, NEW.email_id, NEW.platform_id
-                    , NEW.iter, NEW.cmc_count, NEW.cmc_ok, NEW.core, NEW.workunit_tid, NEW.cmc_last )
+                    , NEW.rc5_iter, NEW.rc5_cmc_count, NEW.rc5_cmc_ok, NEW.core, NEW.workunit_tid, NEW.rc5_cmc_last )
 ;
 
-CREATE OR REPLACE RULE log_r72_insert_8 AS ON INSERT TO log_r72
+CREATE OR REPLACE RULE log_rc5_insert_8 AS ON INSERT TO log_rc5
     WHERE NEW.project_id = 8
     DO INSTEAD
         INSERT INTO log_8( return_time, ip_address, email_id, platform_id
-                    , iter, cmc_count, cmc_ok, core, workunit_tid, cmc_last )
+                    , rc5_iter, rc5_cmc_count, rc5_cmc_ok, core, workunit_tid, rc5_cmc_last )
             VALUES( NEW.return_time, NEW.ip_address, NEW.email_id, NEW.platform_id
-                    , NEW.iter, NEW.cmc_count, NEW.cmc_ok, NEW.core, NEW.workunit_tid, NEW.cmc_last )
+                    , NEW.rc5_iter, NEW.rc5_cmc_count, NEW.rc5_cmc_ok, NEW.core, NEW.workunit_tid, NEW.rc5_cmc_last )
 ;
 
 /*
@@ -52,27 +52,81 @@ CREATE OR REPLACE RULE log_ogr_insert_other AS ON INSERT TO log_ogr
     WHERE NEW.project_id NOT IN ( 24, 25 )
     DO INSTEAD
         INSERT INTO log_ogr_other( project_id, return_time, ip_address, email_id, platform_id
-                    , workunit_tid, nodecount, status )
+                    , workunit_tid, ogr_nodecount, ogr_status )
             VALUES( NEW.project_id, NEW.return_time, NEW.ip_address, NEW.email_id, NEW.platform_id
-                    , NEW.workunit_tid, NEW.nodecount, NEW.status )
+                    , NEW.workunit_tid, NEW.ogr_nodecount, NEW.ogr_status )
 ;
 
 CREATE OR REPLACE RULE log_ogr_insert_24 AS ON INSERT TO log_ogr
     WHERE NEW.project_id = 24
     DO INSTEAD
         INSERT INTO log_24( return_time, ip_address, email_id, platform_id
-                    , workunit_tid, nodecount, status )
+                    , workunit_tid, ogr_nodecount, ogr_status )
             VALUES( NEW.return_time, NEW.ip_address, NEW.email_id, NEW.platform_id
-                    , NEW.workunit_tid, NEW.nodecount, NEW.status )
+                    , NEW.workunit_tid, NEW.ogr_nodecount, NEW.ogr_status )
 ;
 
 CREATE OR REPLACE RULE log_ogr_insert_25 AS ON INSERT TO log_ogr
     WHERE NEW.project_id = 25
     DO INSTEAD
         INSERT INTO log_25( return_time, ip_address, email_id, platform_id
-                    , workunit_tid, nodecount, status )
+                    , workunit_tid, ogr_nodecount, ogr_status )
             VALUES( NEW.return_time, NEW.ip_address, NEW.email_id, NEW.platform_id
-                    , NEW.workunit_tid, NEW.nodecount, NEW.status )
+                    , NEW.workunit_tid, NEW.ogr_nodecount, NEW.ogr_status )
+;
+
+/*
+    GENERIC
+*/
+
+CREATE OR REPLACE VIEW log AS
+    SELECT project_id, return_time, ip_address, email_id, platform_id, workunit_tid
+            , core, rc5_cmc_count, rc5_cmc_ok, rc5_iter, rc5_cmc_last
+            , ogr_status, ogr_nodecount
+        FROM log_other
+    UNION ALL
+    SELECT project_id, return_time, ip_address, email_id, platform_id, workunit_tid
+            , core, rc5_cmc_count, rc5_cmc_ok, rc5_iter, rc5_cmc_last
+            , NULL AS ogr_status, NULL AS ogr_nodecount
+        FROM log_rc5
+    UNION ALL
+    SELECT project_id, return_time, ip_address, email_id, platform_id, workunit_tid
+            , NULL AS core, NULL AS rc5_cmc_count, NULL AS rc5_cmc_ok, NULL AS rc5_iter, NULL AS rc5_cmc_last
+            , ogr_status, ogr_nodecount
+        FROM log_ogr
+;
+
+CREATE OR REPLACE RULE log_insert_nothing AS ON INSERT TO log
+    DO INSTEAD NOTHING
+;
+
+CREATE OR REPLACE RULE log_insert_other AS ON INSERT TO log
+    WHERE NEW.project_id NOT IN ( 8, 24, 25 )
+    DO INSTEAD
+        INSERT INTO log_other( project_id, return_time, ip_address, email_id, platform_id, workunit_tid
+                    , core, rc5_cmc_count, rc5_cmc_ok, rc5_iter, rc5_cmc_last
+                    , ogr_status, ogr_nodecount )
+            VALUES( NEW.project_id, NEW.return_time, NEW.ip_address, NEW.email_id, NEW.platform_id, NEW.workunit_tid
+                    , NEW.core, NEW.rc5_cmc_count, NEW.rc5_cmc_ok, NEW.rc5_iter, NEW.rc5_cmc_last
+                    , NEW.ogr_status, NEW.ogr_nodecount )
+;
+
+CREATE OR REPLACE RULE log_insert_rc5 AS ON INSERT TO log
+    WHERE NEW.project_id NOT IN ( 5, 8 )
+    DO INSTEAD
+        INSERT INTO log_rc5( project_id, return_time, ip_address, email_id, platform_id, workunit_tid
+                    , core, rc5_cmc_count, rc5_cmc_ok, rc5_iter, rc5_cmc_last )
+            VALUES( NEW.project_id, NEW.return_time, NEW.ip_address, NEW.email_id, NEW.platform_id, NEW.workunit_tid
+                    , NEW.core, NEW.rc5_cmc_count, NEW.rc5_cmc_ok, NEW.rc5_iter, NEW.rc5_cmc_last )
+;
+
+CREATE OR REPLACE RULE log_insert_ogr AS ON INSERT TO log
+    WHERE NEW.project_id NOT IN ( 24, 25 )
+    DO INSTEAD
+        INSERT INTO log_ogr( project_id, return_time, ip_address, email_id, platform_id, workunit_tid
+                    , ogr_status, ogr_nodecount )
+            VALUES( NEW.project_id, NEW.return_time, NEW.ip_address, NEW.email_id, NEW.platform_id, NEW.workunit_tid
+                    , NEW.ogr_status, NEW.ogr_nodecount )
 ;
 
 -- vi: expandtab ts=4 sw=4
