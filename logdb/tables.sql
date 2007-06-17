da0c50930e90a570c098facef6d3cac2a09f45b1
decibel <decibel>
2007-06-17 03:55:33 +0000

    Remove constraint names (defaults are good enough), ensure constraints are consistent

--- logdb/tables.sql
+++ logdb/tables.sql
@@ -1,4 +1,4 @@
--- $Id: tables.sql,v 1.12 2007/06/17 03:47:34 decibel Exp $
+-- $Id: tables.sql,v 1.13 2007/06/17 03:55:33 decibel Exp $
 
 BEGIN;
 CREATE TABLE log_type (
@@ -34,24 +34,6 @@ CREATE TABLE import (
 	, bad_ip_address	text
 ) WITHOUT OIDs;
 
-CREATE TABLE log_other (
-	return_time		timestamp NOT NULL
-	, email_id		int NOT NULL
-	, platform_id		integer NOT NULL
-	, ip_address		inet
-	, core			integer
-	, rc5_cmc_count		integer
-	, project_id		smallint NOT NULL
-	, log_type_id		smallint NOT NULL
-	, rc5_iter		smallint
-	, ogr_status		smallint
-	, rc5_cmc_ok		smallint
-	, ogr_nodecount		bigint
-	, workunit_tid		text NOT NULL
-	, rc5_cmc_last		text
-	, bad_ip_address	text
-) WITHOUT OIDs;
-
 CREATE TABLE email (
 	email_id	serial 		PRIMARY KEY
 	, email		varchar(64) 	NOT NULL UNIQUE
@@ -63,6 +45,24 @@ CREATE TABLE platform (
 	, cpu		int	NOT NULL
 	, version	int	NOT NULL
 	, UNIQUE (os, cpu, version)
+) WITHOUT OIDs;
+
+CREATE TABLE log_other (
+	return_time		timestamp NOT NULL
+	, email_id		integer NOT NULL REFERENCES email
+	, platform_id		integer NOT NULL REFERENCES platform
+	, ip_address		inet
+	, core			integer
+	, rc5_cmc_count		integer
+	, project_id		smallint NOT NULL
+	, log_type_id		smallint NOT NULL REFERENCES log_type
+	, rc5_iter		smallint
+	, ogr_status		smallint
+	, rc5_cmc_ok		smallint
+	, ogr_nodecount		bigint
+	, workunit_tid		text NOT NULL
+	, rc5_cmc_last		text
+	, bad_ip_address	text
 ) WITHOUT OIDs;
 
 /*
@@ -78,7 +78,7 @@ CREATE TABLE log_8 (
 	, rc5_cmc_ok		smallint
 	, rc5_iter		smallint NOT NULL
 	, core			smallint NOT NULL
-	, log_type_id		smallint NOT NULL
+	, log_type_id		smallint NOT NULL REFERENCES log_type
 	, workunit_tid		varchar(20) NOT NULL
 	, rc5_cmc_last		varchar(20)
 	, bad_ip_address	text
@@ -88,8 +88,9 @@ CREATE INDEX log_8__email_id ON log_8( email_id );
 CREATE TABLE log_rc5_other WITHOUT OIDs AS
 	SELECT 0::smallint AS project_id, * FROM log_8
 ;
-ALTER TABLE log_rc5_other ADD CONSTRAINT log_rc5_other__email_ri FOREIGN KEY ( email_id ) REFERENCES email;
-ALTER TABLE log_rc5_other ADD CONSTRAINT log_rc5_other__platform_ri FOREIGN KEY ( platform_id ) REFERENCES platform;
+ALTER TABLE log_rc5_other ADD FOREIGN KEY ( email_id ) REFERENCES email;
+ALTER TABLE log_rc5_other ADD FOREIGN KEY ( platform_id ) REFERENCES platform;
+ALTER TABLE log_rc5_other ADD FOREIGN KEY ( log_type_id ) REFERENCES log_type;
 CREATE INDEX log_rc5_other__email_id ON log_rc5_other( project_id, email_id );
 
 /*
@@ -98,11 +99,11 @@ CREATE INDEX log_rc5_other__email_id ON log_rc5_other( project_id, email_id );
 
 CREATE TABLE log_24 (
 	return_time		timestamp NOT NULL
-	, email_id		integer NOT NULL CONSTRAINT log_24__email_ri REFERENCES email
-	, platform_id		integer NOT NULL CONSTRAINT log_24__platform_ri REFERENCES platform
+	, email_id		integer NOT NULL REFERENCES email
+	, platform_id		integer NOT NULL REFERENCES platform
 	, ip_address		inet
 	, ogr_status		smallint
-	, log_type_id		smallint NOT NULL
+	, log_type_id		smallint NOT NULL REFERENCES log_type
 	, ogr_nodecount		bigint NOT NULL
 	, workunit_tid		text NOT NULL
 	, bad_ip_address	text
@@ -112,15 +113,17 @@ CREATE INDEX log_24__email_id ON log_24( email_id );
 CREATE TABLE log_25 WITHOUT OIDs AS
 	SELECT * FROM log_24
 ;
-ALTER TABLE log_25 ADD CONSTRAINT log_25__email_ri FOREIGN KEY ( email_id ) REFERENCES email;
-ALTER TABLE log_25 ADD CONSTRAINT log_25__platform_ri FOREIGN KEY ( platform_id ) REFERENCES platform;
+ALTER TABLE log_25 ADD FOREIGN KEY ( email_id ) REFERENCES email;
+ALTER TABLE log_25 ADD FOREIGN KEY ( platform_id ) REFERENCES platform;
+ALTER TABLE log_25 ADD FOREIGN KEY ( log_type_id ) REFERENCES log_type;
 CREATE INDEX log_25__email_id ON log_25( email_id );
 
 CREATE TABLE log_ogr_other WITHOUT OIDs AS
 	SELECT 0::smallint AS project_id, * FROM log_25
 ;
-ALTER TABLE log_ogr_other ADD CONSTRAINT log_ogr_other__email_ri FOREIGN KEY ( email_id ) REFERENCES email;
-ALTER TABLE log_ogr_other ADD CONSTRAINT log_ogr_other__platform_ri FOREIGN KEY ( platform_id ) REFERENCES platform;
+ALTER TABLE log_ogr_other ADD FOREIGN KEY ( email_id ) REFERENCES email;
+ALTER TABLE log_ogr_other ADD FOREIGN KEY ( platform_id ) REFERENCES platform;
+ALTER TABLE log_ogr_other ADD FOREIGN KEY ( log_type_id ) REFERENCES log_type;
 CREATE INDEX log_ogr_other__email_id ON log_ogr_other( project_id, email_id );
 
 /*
@@ -133,7 +136,7 @@ CREATE INDEX log_ogr_other__email_id ON log_ogr_other( project_id, email_id );
 CREATE TABLE log_history (
 	log_day			date NOT NULL
 	, log_hour		smallint NOT NULL
-	, log_type_id		smallint NOT NULL REFERENCES log_type( log_type_id )
+	, log_type_id		smallint NOT NULL REFERENCES log_type
 	, lines			integer NOT NULL
 	, start_time		timestamp NOT NULL
 	, end_time		timestamp NOT NULL
