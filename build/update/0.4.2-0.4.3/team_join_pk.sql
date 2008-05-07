0b6c9391f3e2740762f060a4362b6d213660194a
decibel <decibel>
2008-05-07 14:50:52 +0000

    Fix think-o

--- build/update/0.4.2-0.4.3/team_join_pk.sql
+++ build/update/0.4.2-0.4.3/team_join_pk.sql
@@ -1,5 +1,5 @@
 BEGIN;
-    ALTER TABLE team_joins DROP team_joins_pkey;
+    ALTER TABLE team_joins DROP CONSTRAINT team_joins_pkey;
     ALTER TABLE team_joins ADD PRIMARY KEY( id, join_date );
 
     UPDATE version SET version='0.4.3' WHERE component='schema';
