e2fbc7fd4cd44d290c26b1139bd8f8975cf85598
decibel <decibel>
2008-05-09 13:33:16 +0000

    Allow processing to insert into team_joins

--- build/update/0.4.2-0.4.3/team_join_pk.sql
+++ build/update/0.4.2-0.4.3/team_join_pk.sql
@@ -1,6 +1,7 @@
 BEGIN;
     ALTER TABLE team_joins DROP CONSTRAINT team_joins_pkey;
     ALTER TABLE team_joins ADD PRIMARY KEY( id, join_date );
+    GRANT INSERT ON team_joins TO GROUP processing;
 
     UPDATE version SET version='0.4.3' WHERE component='schema';
 COMMIT;
