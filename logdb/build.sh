4cd09214c2f540d856377b710542a7cc25d94746
nerf <nerf>
2009-04-22 06:05:10 +0000

    Add database creation bits

--- logdb/build.sh
+++ logdb/build.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 #
-# $Id: build.sh,v 1.3 2007/10/29 00:38:48 decibel Exp $
+# $Id: build.sh,v 1.4 2009/04/22 06:05:10 nerf Exp $
+
+# uncomment these if you need to first create the database
+# createdb -E UTF8 logs
+# createlang plpgsql logs
 
 load () {
     psql -f $1 logs
